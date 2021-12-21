c455693027df38dc83ccf65c0ca020bab15b55c5
saito.yuya <saito.yu@uhmfg.co.jp>
2021-12-21 21:50:50 +0900

    change base setting

--- src/app/settings.py
+++ src/app/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_local',
+        'USER': 'django_user',
+        'PASSWORD': 'secret',
+        'HOST': 'db',
+        'POST': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
